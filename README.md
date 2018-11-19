c1cc2190a0bb98f5c8ab90892c207b09b9d19f5c
Sergio <sergiopiermario.placanica@mail.polimi.it>
2018-11-19 01:39:49 +0100

    GUI skeleton

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# trackme
-trackme implementation
+# track_me
+
+TrackMe application implementation
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/styles/texts.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+Map<String, TextStyle> textStyles = {
+  'title_text': new TextStyle(
+    fontSize: 28.0,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.bold,
+    color: colorStyles['text_color'],
+  ),
+
+  'normal_text': new TextStyle(
+    fontSize: 24.0,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.normal,
+    color: colorStyles['text_color'],
+  ),
+
+};
